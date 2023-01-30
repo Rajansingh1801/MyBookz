11b84b4f132b28397809e69819640c1bfeb8c81d
Rajan singh <87349777+Rajansingh1801@users.noreply.github.com>
2023-01-30 16:52:43 +0530

    m1

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,12 +1,85 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Linking,
+} from "react-native";
 import React from "react";
 import Menubar from "../component/menu_nav";
 
 const About = () => {
   return (
     <>
-      <View>
-        <Text>About</Text>
+      <View style={{ backgroundColor: "#3925A8" }}>
+        <View style={styles.maincont}>
+          <View>
+            <Image
+              source={require("../../assets/rajan.png")}
+              style={styles.img}
+              resizeMode="contain"
+            />
+          </View>
+          <Text style={styles.txt}>About Me</Text>
+          <Text style={styles.txt1}></Text>
+          <Text style={styles.txt2}>
+            I am web developer and web design and these App is made by React
+            native.
+          </Text>
+          <View style={styles.social}>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL("https://www.facebook.com/rajansinghnahne")
+              }
+            >
+              <Image
+                source={require("../../assets/social/facebook.png")}
+                resizeMode="contain"
+                style={styles.logo}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.instagram.com/rajan_singh_raj/?igshid=Mzc0YWU1OWY%3D"
+                )
+              }
+            >
+              <Image
+                source={require("../../assets/social/instagram.png")}
+                resizeMode="contain"
+                style={styles.logo}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.linkedin.com/in/rajan-singh-5b5090228/"
+                )
+              }
+            >
+              <Image
+                source={require("../../assets/social/linkedin.png")}
+                resizeMode="contain"
+                style={styles.logo}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                Linking.openURL(
+                  "https://twitter.com/rajansinghnahn1?t=6lWdrnUuYgrXiWUg8Ou_fg&s=09"
+                )
+              }
+            >
+              <Image
+                source={require("../../assets/social/twitter.png")}
+                resizeMode="contain"
+                style={styles.logo}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
       <Menubar />
     </>
@@ -15,4 +88,32 @@ const About = () => {
 
 export default About;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  maincont: {
+    justifyContent: "center",
+    // alignItems: "center",
+    height: "100%",
+    padding: 30,
+  },
+  img: {
+    width: "100%",
+    height: 400,
+  },
+  txt: {
+    textTransform: "uppercase",
+    color: "white",
+    fontWeight: "900",
+  },
+  txt1: { width: 50, height: 2, backgroundColor: "white", marginTop: 3 },
+  txt2: {
+    color: "white",
+    paddingVertical: 15,
+  },
+  social: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  logo: { width: 40, height: 40, marginHorizontal: 10 },
+});
--- src/screens/contactus.js
+++ src/screens/contactus.js
@@ -1,13 +1,88 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import Checkbox from "expo-checkbox";
+import React, { useState } from "react";
 import Menubar from "../component/menu_nav";
 
 const Contact = () => {
+  const [ispress, setpress] = useState(false);
+  const [ischeck, setcheck] = useState(false);
   return (
     <>
-      <View>
-        <Text>Contact</Text>
-      </View>
+       <View style={styles.cont}>
+        <View>
+          <Text style={styles.txt1}>
+            Contact us{"\n"}Any where,{"\n"}Any time
+          </Text>
+          <SafeAreaView>
+            <View>
+              <Text style={styles.txt2}>Enter your name</Text>
+              <TextInput
+                keyboardType="ascii-capable"
+                onPressIn={() => setpress(!ispress)}
+                style={[
+                  styles.input,
+                  { borderColor: ispress ? "gray" : "blue" },
+                ]}
+              />
+            </View>
+            <View>
+              <Text style={styles.txt2}>Enter Email</Text>
+              <TextInput
+                onPressIn={() => setpress(!ispress)}
+                style={[
+                  styles.input,
+                  { borderColor: ispress ? "gray" : "blue" },
+                ]}
+                keyboardType="email-address"
+              />
+            </View>
+            <View>
+              <Text style={styles.txt2}>Enter Your Mobile Mo.</Text>
+              <TextInput
+                onPressIn={() => setpress(!ispress)}
+                style={[
+                  styles.input,
+                  { borderColor: ispress ? "gray" : "blue" },
+                ]}
+                keyboardType="numeric"
+              />
+            </View>
+            <View>
+              <Text style={styles.txt2}>How can we help you ?</Text>
+              <TextInput
+                onPressIn={() => setpress(!ispress)}
+                multiline
+                numberOfLines={4}
+                style={[
+                  styles.textarea,
+                  { borderColor: ispress ? "gray" : "blue" },
+                ]}
+                keyboardType="ascii-capable"
+              />
+            </View>
+            <View style={{ flexDirection: "row", marginVertical: 15 }}>
+              <Checkbox
+                value={ischeck}
+                onValueChange={() => {
+                  setcheck(!ischeck);
+                }}
+                style={styles.checkbox}
+              />
+              <Text style={{ paddingStart: 10 }}>I have read all T&C</Text>
+            </View>
+            <TouchableOpacity style={styles.submit}>
+              <Text style={styles.subtxt}>Contact Us</Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+        </View>
+      </View> 
       <Menubar />
     </>
   );
@@ -15,4 +90,41 @@ const Contact = () => {
 
 export default Contact;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cont: { backgroundColor: "#F3EFE0", paddingHorizontal: 30, height: "100%" },
+  txt1: { fontSize: 32, fontWeight: "800" },
+  input: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    // borderColor: "gray",
+    fontSize: 18,
+    paddingStart: 12,
+    paddingVertical: 7,
+    borderRadius: 6,
+  },
+  textarea: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    // borderColor: "gray",
+    fontSize: 18,
+    paddingStart: 12,
+    paddingVertical: 7,
+    borderRadius: 6,
+  },
+  txt2: { fontSize: 18, fontWeight: "600", marginTop: 25, marginBottom: 10 },
+  checkbox: { height: 20, width: 20, borderColor: "black", borderWidth: 1 },
+  submit: {
+    backgroundColor: "green",
+    marginHorizontal: 30,
+    marginVertical: 20,
+    marginBottom: 60,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  subtxt: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "white",
+    fontWeight: "800",
+  },
+});
